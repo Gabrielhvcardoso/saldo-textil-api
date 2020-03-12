b2183c981eecf54b33cf392368417eea7e2241e1
Gabriel Cardoso <gabrielhvcardoso@gmail.com>
2020-03-12 10:50:14 -0300

    reparos

--- src/index.js
+++ src/index.js
@@ -19,6 +19,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.PORT || 2424, function() {
-  console.log("\nWorking on http://localhost:2424\n")
-})
+app.listen(process.env.PORT || 2424)
